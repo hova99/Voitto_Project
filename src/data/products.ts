904ef588c4add374a7c9ce62df151c93ea0b3c04
hova99 <console564ken@gmail.com>
2025-08-01 16:52:01 +0300

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -48,7 +48,7 @@ export const products: Product[] = [
     image:
       "https://res.cloudinary.com/dnv6mjhxv/image/upload/v1754056277/shall-removebg-preview_wu9gzw_prx95u.webp",
     image2:
-      "https://res.cloudinary.com/dnv6mjhxv/image/upload/f_auto,q_auto,w_400,c_scale/v1753511986/shall-removebg-preview_wu9gzw.png",
+      "https://res.cloudinary.com/dnv6mjhxv/image/upload/v1754056277/shall-removebg-preview_wu9gzw_prx95u.webp",
     category: "drainage",
     description:
       "Used to control surface water flow along roads and compounds.",
